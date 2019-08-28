d59e489d48602daaec69a22447668e08fdad8cf4
Alex <alexandra.vandenbilcke@gmail.com>
2019-08-28 18:37:05 +0200

    advanced but not finished, do not merge

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,11 +1,25 @@
-class Api::V1::ImagesController < Api::V1::BaseController
-  def index
+require 'json'
+require 'open-uri'
 
-    image = Image.from(params[:url])
-    if image == true
-      "Blur this image"
-    else
-      "call API"
+class Api::V1::ImagesController < Api::V1::BaseController
+
+  def index
+    dom = '#'
+    image_serialized = open(dom).read
+    images = JSON.parse(image_serialized)
+
+    images.each do |img|
+      url = Image.from(img[:url])
+      keyword = Image.verify(img[:keyword])
+      if url == false
+        # APPELER API
+      elsif url == true && keyword == false
+        @alert = false
+      elsif url == true && keyword == true
+        @alert = true
+      else
+        "ERROR"
+      end
     end
   end
 
--- app/models/image.rb
+++ app/models/image.rb
@@ -5,4 +5,10 @@ class Image < ApplicationRecord
       Image.find(image.id).urls.include?(url)
     end
   end
+
+  def self.verify(keyword)
+    @images.each do |image|
+      Image.find(image.id).keywords.include?(keyword)
+    end
+  end
 end
