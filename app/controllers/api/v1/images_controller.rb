db51cd9f4983c0ba41bede9b7b5a15782d4f2a23
Alex <alexandra.vandenbilcke@gmail.com>
2019-08-28 19:39:36 +0200

    Render a JSON... But that's it. Don't Merge

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -4,22 +4,37 @@ require 'open-uri'
 class Api::V1::ImagesController < Api::V1::BaseController
 
   def index
-    dom = '#'
-    image_serialized = open(dom).read
-    images = JSON.parse(image_serialized)
+    # dom = '#'
+    # image_serialized = open(dom).read
+    # images = JSON.parse(image_serialized)
+
+    images = [
+      {
+        url: "https://img.lemde.fr/2016/12/01/0/0/2250/1500/688/0/60/0/d0530c1_7640-1p0efxb.4ogsnhfr.JPG",
+        keyword: "spider"
+      },
+      {
+        url: "https://test.jpg",
+        keyword: "spider"
+      },{
+        url: "https://img.lemde.fr/2016/12/01/0/0/2250/1500/688/0/60/0/d0530c1_7640-1p0efxb.4ogsnhfr.JPG",
+        keyword: "snake"
+      }
+    ]
+
+    @responses = []
 
     images.each do |img|
       url = Image.from(img[:url])
       keyword = Image.verify(img[:keyword])
-      if url == false
+      # if url == false
         # APPELER API
-      elsif url == true && keyword == false
+      if url == true && keyword == false
         @alert = false
       elsif url == true && keyword == true
         @alert = true
-      else
-        "ERROR"
       end
+      @responses << { url: img[:url].to_s, alert: @alert.to_s }
     end
   end
 
--- app/views/api/v1/images/index.json.jbuilder
+++ app/views/api/v1/images/index.json.jbuilder
@@ -1,6 +1,6 @@
-# json.array! @images do |image|
-#   json.extract! image, :id, :alert
-# end
+json.array! @responses do |response|
+  json.extract! response, :url, :alert
+end
 
 # Il va falloir faire un map
 # output => [{ id: 'toto', alert: true }, { ... }]
